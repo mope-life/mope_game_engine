d49de65b9c8f0262cf587a94a3305b53cd3b65e6
mope-life <dustinross@live.com>
2026-02-03 18:51:58 -0800

    Make `event_pool::process_event()` public
    `class game_scene` needs to name this function template as a friend, so
    it needs to be public.
    
    Interestingly, MSVC had no problem compiling this, but GCC caught it
    (correctly, I think). Another day, another bug report!:
    https://developercommunity.visualstudio.com/t/Friend-declaration-naming-a-function-tem/11038438

--- include/mope_game_engine/game_scene.hxx
+++ include/mope_game_engine/game_scene.hxx
@@ -31,12 +31,17 @@ namespace mope
         std::vector<std::tuple<void*, void(*)(game_scene&, void*)>> m_events;
         std::pmr::unsynchronized_pool_resource m_pool;
 
+    public:
         template <typename Event>
         static void process_event(game_scene& scene, void* ptr);
 
-    public:
         template <typename Event, typename... Args>
-        void store(Args&&... args);
+        void store(Args&&... args)
+        {
+            auto ptr = m_pool.allocate(sizeof(Event), alignof(Event));
+            auto event = new (ptr) Event(std::forward<Args>(args)...);
+            m_events.emplace_back(static_cast<void*>(event), &process_event<Event>);
+        }
 
         auto events() -> decltype(m_events) const&;
         void clear();
@@ -276,13 +281,5 @@ namespace mope
         }
 
         scene.m_event_pool.m_pool.deallocate(ptr, sizeof(Event), alignof(Event));
-    }
-
-    template <typename Event, typename... Args>
-    void event_pool::store(Args&&... args)
-    {
-        auto ptr = m_pool.allocate(sizeof(Event), alignof(Event));
-        auto event = new (ptr) Event(std::forward<Args>(args)...);
-        m_events.emplace_back(static_cast<void*>(event), &process_event<Event>);
     }
 }
